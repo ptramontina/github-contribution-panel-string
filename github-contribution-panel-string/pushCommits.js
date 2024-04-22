b51c78bb9e9931c1ad4ca51e8abb6f6ea4c906f8
Pedro Ernesto Tramontina <pedro.tramontina@gmail.com>
2024-04-22 17:22:22 -0300

    Add retry logic

--- github-contribution-panel-string/pushCommits.js
+++ github-contribution-panel-string/pushCommits.js
@@ -13,26 +13,32 @@ async function pushCommits() {
     for (let i = 0; i < commitNumber; i++) {
       let response = await axiosInstance.get(`/repos/${gitUser}/${gitRepo}/contents/${gitPath}`);
       let sha = response.data.sha;
-
-      try {
-        let currentTime = moment();
-        
-        console.log(`Executing commit ${i+1} from ${commitNumber} at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`);
-    
-        let putResponse = await axiosInstance.put(`/repos/${gitUser}/${gitRepo}/contents/${gitPath}`, {
-          content: Buffer.from(`Commit at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`).toString('base64'),
-          message: `Commit ${(i + 1)} of the day, at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`,
-          sha
-        });
-  
-        console.log('Response from PUT: ', putResponse.data);
-    
-        console.log(`Commit at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`);
       
-      } catch (e) {
-        console.error(`Error while pushing commits: ${e}`);
+      let attempts = 0
+      const totalAttempts = 3
+      let success = false
+      
+      while (attempts++ < totalAttempts && !success) {
+        try {
+          let currentTime = moment();
+          
+          console.log(`Executing commit ${i+1} from ${commitNumber} at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`);
+      
+          let putResponse = await axiosInstance.put(`/repos/${gitUser}/${gitRepo}/contents/${gitPath}`, {
+            content: Buffer.from(`Commit at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`).toString('base64'),
+            message: `Commit ${(i + 1)} of the day, at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`,
+            sha
+          });
+    
+          console.log('Response from PUT: ', putResponse.data);
+      
+          console.log(`Commit at ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`);
+          
+          success = true
+        } catch (e) {
+          console.error(`Error while pushing commits: ${e}`);
+        } 
       }
-      
     }
   
 }
